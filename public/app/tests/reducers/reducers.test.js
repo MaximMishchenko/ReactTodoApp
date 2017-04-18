723f5c51c5cd002933c860fba150da58897a931a
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-04-18 23:50:59 +0300

    Add todos reducer and tests

--- public/app/tests/reducers/reducers.test.js
+++ public/app/tests/reducers/reducers.test.js
@@ -1,6 +1,6 @@
 import expect from 'expect'
 import deepFreeze from 'deep-freeze-strict'
-import { searchTextReducer, showCompletedReducer } from 'reducers'
+import { searchTextReducer, showCompletedReducer, todosReducer } from 'reducers'
 
 describe('Reducers', () => {
 	describe('searchTextReducer', () => {
@@ -25,6 +25,41 @@ describe('Reducers', () => {
 			let res = showCompletedReducer(deepFreeze(false), deepFreeze(action))
 
 			expect(res).toEqual(true)
+		})
+	})
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			let action = {
+				type: 'ADD_TODO',
+				text: 'Some todo text'
+			}
+
+			let res = todosReducer(deepFreeze([]), deepFreeze(action))
+
+			expect(res.length).toEqual(1)
+			expect(res[0].text).toEqual(action.text)
+		})
+
+		it('should toggle todo', () => {
+			let todos = [
+				{
+					id: 2,
+			        text: 'some other todo',
+			        completed: true,
+			        createdAt: 123,
+			        completedAt: 125
+				}
+			]
+			let action = {
+				type: 'TOGGLE_TODO',
+				id: 2
+			}
+
+			let res = todosReducer(deepFreeze(todos), deepFreeze(action))
+
+			expect(res[0].completedAt).toEqual(undefined)
+			expect(res[0].completed).toEqual(false)
 		})
 	})
 })
