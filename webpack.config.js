d3fd901785df20e3dc72b0c563cdccbe44b606f8
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-04-26 23:34:50 +0300

    Run webpack on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: [
     'script-loader!jquery/dist/jquery.min.js',
@@ -13,6 +15,11 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
     })
   ],
   output: {
@@ -43,5 +50,6 @@ module.exports = {
       }
     ]
   },
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map',
   watch: true
 };
